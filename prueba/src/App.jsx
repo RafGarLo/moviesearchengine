584b9be3027a4033a626c0e92ddc6c1fedb91c04
Rafael <grafa74@gmail.com>
2023-02-16 18:16:16 +0100

    created handlesubmit to save user input

--- prueba/src/App.jsx
+++ prueba/src/App.jsx
@@ -1,19 +1,28 @@
 import "./App.css";
 import { Movies } from "./components/movies";
-import {useMovies} from './hooks/useMovies'
-
-
-
+import { useMovies } from "./hooks/useMovies";
+import { useRef } from "react";
 
 function App() {
-  const { movies: mappedMovies } = useMovies()
+  const { movies: mappedMovies } = useMovies();
+  const inputRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const inputEl = inputRef.current;
+    const value = inputEl.value;
+    console.log(value);
+  };
 
   return (
     <div className="page">
       <header>
         <h1>Buscador de películas</h1>
-        <form className="form">
-          <input placeholder="Avengers, Star Wars, The Matrix..." />
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            ref={inputRef}
+            placeholder="Avengers, Star Wars, The Matrix..."
+          />
           <button type="submit">Buscar</button>
         </form>
       </header>
